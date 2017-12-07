0c5dc24c62c78f14d9d14d70a68983a6d368280b
mawills <wattmills@gmail.com>
2017-12-07 12:14:56 -0500

    bootstrapped static versions of components to be made dynamic later

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe 1', 'This is a test guy 1', 'http://test-ipv6.com/images/hires_offline.png'),
+    new Recipe('A Test Recipe 2', 'This is a test guy 2', 'http://test-ipv6.com/images/hires_offline.png')
+  ];
 
   constructor() { }
 
